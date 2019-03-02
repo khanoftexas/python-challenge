6e9bf023721d6d24edc758e224a8c05c4f00a2c9
khanoftexas <khanoftexas@gmail.com>
2019-03-02 17:42:36 -0600

    Python challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,38 +9,49 @@ budget_csv = os.path.join("..","Resources","budget_data.csv")
 with open(budget_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     
-    # Counts the Total Rows
-    rows_values=[r for r in csvreader]
-    # Default values starting from row 1 bypassing headers
-    profit_change = 0 #int(rows_values[1][1])
+    # Skip headers
+    next(csvreader, None)
+
+    # Read data
+    row = next(csvreader,None)
+    max_month = row[0]
+    min_month = row[0]
+    profitloss = int(row[1])
+    min_profitloss = profitloss
+    max_profitloss = profitloss
+    previous_profitloss = profitloss
+    months_count = 1
+    total_profit_loss = profitloss
+    profitloss_change_sum = 0
     
-    greatest_increase_row=rows_values[1]
-    greatest_decrease_row=rows_values[1]
+    for row in csvreader:
+            months_count = months_count + 1
 
-    #Looping to calculate number of months, profit change, minimum, maximum
-    for i in range(1,len(rows_values)):
-        months_count=months_count+1
-        row=rows_values[i]
-        total_profit_loss= total_profit_loss + int(row[1]) 
+            profitloss = int(row[1])
+            total_profit_loss = total_profit_loss + profitloss
 
-        if i > 1:
-            profit_change=profit_change + (int(row[1])-int(rows_values[i-1][1]))
-         #Finding Max and Min Profits
-        if int(greatest_increase_row[1]) < int(row[1]):
-            greatest_increase_row=row
+            profit_change = profitloss - previous_profitloss
 
-        if int(greatest_decrease_row[1]) > int(row[1]):
-            greatest_decrease_row=row  
+            profitloss_change_sum = profitloss_change_sum + profit_change
 
-    average_profit_loss= int(total_profit_loss /months_count)
-    average_profit_change=int(profit_change/months_count)
+            if profit_change > max_profitloss:
+                max_month = row[0]
+                max_profitloss = profit_change
+
+            if profit_change < min_profitloss:
+                min_month = row[0]
+                min_profitloss = profit_change
+
+            previous_profitloss = profitloss
+
+    average_profit_change=round(float(profitloss_change_sum/(months_count-1)),2)
     print("Financial Analysis")
     print("----------------------------")
     print(f"Total Months: {months_count}")
     print(f"Total: ${total_profit_loss}")
     print(f"Average Change: $ {average_profit_change}")
-    print(f"Greatest Increase in Profits: {greatest_increase_row[0]} $ {greatest_increase_row[1]}")
-    print(f"Greatest Decrease in Profits: {greatest_decrease_row[0]} $ {greatest_decrease_row[1]}")
+    print(f"Greatest Increase in Profits: {max_month} $ {max_profitloss}")
+    print(f"Greatest Decrease in Profits: {min_profitloss} $ {min_profitloss}")
   
   
     output_file = open("PyBank.txt","w") 
@@ -50,8 +61,8 @@ output_file.write("----------------------------"+ '\n')
 output_file.write(f"Total Months: {months_count}"+ '\n')
 output_file.write(f"Total: ${total_profit_loss}"+ '\n')
 output_file.write(f"Average Change: ${average_profit_change}"+ '\n')
-output_file.write(f"Greatest Increase in Profits: {greatest_increase_row[0]} $ {greatest_increase_row[1]}"+ '\n')
-output_file.write(f"Greatest Decrease in Profits: {greatest_decrease_row[0]} $ {greatest_decrease_row[1]}"+ '\n')
+output_file.write(f"Greatest Increase in Profits: {max_month} $ {max_profitloss}"+ '\n')
+output_file.write(f"Greatest Decrease in Profits: {min_profitloss} $ {min_profitloss}"+ '\n')
  
 output_file.close() 
 
