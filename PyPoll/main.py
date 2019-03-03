7496099284de33b374bedc08dc925d5255e826dd
khanoftexas <khanoftexas@gmail.com>
2019-03-02 19:29:42 -0600

    Python challenge

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,36 +1,52 @@
 import os
 import csv
+import operator
 #Initialize Variable
 vote_count=0
+dashes="-------------------------"
+candidates = []
+vote_count_candidate = {}
 
 election_csv = os.path.join("..","Resources","election_data.csv")
 
 # Open the CSV
 with open(election_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader)
 
     for row in csvreader:
         vote_count = vote_count + 1
+        if row[2] not in candidates:
+            candidates.append(row[2])
+            vote_count_candidate[row[2]] = 1
+        else:
+            vote_count_candidate[row[2]] = vote_count_candidate[row[2]] + 1
+
+
+
+winner = max(vote_count_candidate.items(), key=operator.itemgetter(1))[0]
 
 print(f"Election Results") 
-print(f"-------------------------")
+print(f"{dashes}")
 print(f"Total Votes: {vote_count}")
-print(f"-------------------------")
-
-print(f"-------------------------")   
-print(f"Winner: "+ '\n')
-print(f"-------------------------")
+print(f"{dashes}")
+for c, v in vote_count_candidate.items():
+    print(c, "{:.3f}".format(round(((v/vote_count)*100),3)) + "%","(",(str(v)),")")
+print(f"{dashes}")   
+print(f"Winner: "+ str(winner) +'\n')
+print(f"{dashes}")
 
 
 
 output_file = open("PyPoll.txt","w") 
  
 output_file.write("Election Results"+ '\n') 
-output_file.write("-------------------------"+ '\n')
+output_file.write(f"{dashes}"+ '\n')
 output_file.write(f"Total Votes: {vote_count}"+ '\n')
-output_file.write("-------------------------"+ '\n')
-
-output_file.write("-------------------------"+ '\n')   
-output_file.write(f"Winner: "+ '\n')
-output_file.write("-------------------------"+ '\n')
+output_file.write(f"{dashes}"+ '\n')
+for c, v in vote_count_candidate.items():   
+    output_file.write(c+" "+"{:.3f}".format(round(((v/vote_count)*100),3)) + "%"+"  ("+(str(v))+")"+ '\n')
+output_file.write(f"{dashes}"+ '\n')   
+output_file.write(f"Winner: " + str(winner) + '\n')
+output_file.write(f"{dashes}"+ '\n')
 output_file.close() 
